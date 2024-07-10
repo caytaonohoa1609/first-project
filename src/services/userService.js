7a7b674f89f37b32d2c8b8f7f247e8a84a69a0d8
caytaonohoa1609 <164864433+caytaonohoa1609@users.noreply.github.com>
2024-07-10 16:08:50 +0700

    Load Động Data Chọn Lịch Khám Bệnh Theo Thời Gian Ngày Tạo

--- src/services/userService.js
+++ src/services/userService.js
@@ -50,6 +50,11 @@ const saveBulkScheduleDoctor = (data) => {
     return axios.post('/api/bulk-create-schedule', data)
 }
 
+
+const getScheduleDoctorByDate = (doctorId, date) => {
+    return axios.get(`/api/get-schedule-doctor-by-date?doctorId=${doctorId}&date=${date}`)
+}
+
 export { 
         handleLoginApi, 
         getAllUsers, 
@@ -61,5 +66,6 @@ export {
         getAllDoctors,
         saveDetailDoctorService,
         getDetailInforDoctor,
-        saveBulkScheduleDoctor
+        saveBulkScheduleDoctor,
+        getScheduleDoctorByDate
 }
