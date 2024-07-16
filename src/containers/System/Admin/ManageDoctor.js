695410ab82c04a38f2e5573b58a3cd9d86d922ea
caytaonohoa1609 <164864433+caytaonohoa1609@users.noreply.github.com>
2024-07-16 16:11:18 +0700

    Design Giao Diện Hiển Thị Thông Tin Khám Bệnh Của Bác Sĩ

--- src/containers/System/Admin/ManageDoctor.js
+++ src/containers/System/Admin/ManageDoctor.js
@@ -156,18 +156,57 @@ class ManageDoctor extends Component {
         let res = await getDetailInforDoctor(selectedOption.value);
         if(res && res.errCode === 0 && res.data && res.data.Markdown) {
             let markdown = res.data.Markdown;
+            let { listPayment, listPrice, listProvince } = this.state;
+            let addressClinic = '', nameClinic = '', note = '',
+            paymentId = '', priceId = '', provinceId = '',
+            selectedPayment = '', selectedPrice = '', selectedProvince = '';
+
+            
+
+            if(res.data.Doctor_Infor) {
+                addressClinic = res.data.Doctor_Infor.addressClinic;
+                nameClinic = res.data.Doctor_Infor.nameClinic;
+                note = res.data.Doctor_Infor.note;
+
+                paymentId = res.data.Doctor_Infor.paymentId;
+                priceId = res.data.Doctor_Infor.priceId;
+                provinceId = res.data.Doctor_Infor.provinceId;
+
+
+                selectedPayment = listPayment.find(item => {
+                    return item && item.value === paymentId
+                })
+                selectedPrice = listPrice.find(item => {
+                    return item && item.value === priceId
+                })
+                selectedProvince = listProvince.find(item => {
+                    return item && item.value === provinceId
+                })
+            }
+
+
             this.setState({
                 contentHTML: markdown.contentHTML,
                 contentMarkdown: markdown.contentMarkdown,
                 description: markdown.description,
                 hasOldData: true,
+                addressClinic: addressClinic,
+                nameClinic: nameClinic,
+                note: note,
+                selectedPayment: selectedPayment,
+                selectedPrice: selectedPrice,
+                selectedProvince: selectedProvince
+
             })  
-        }if(res.data.Markdown.contentHTML === null && res.data.Markdown.contentMarkdown === null && res.data.Markdown.description === null) {
+        }else{
             this.setState({
                 contentHTML: '',
                 contentMarkdown: '',
                 description: '',
                 hasOldData: false,
+                addressClinic: '',
+                nameClinic: '',
+                note: ''
             })
         }
     }
